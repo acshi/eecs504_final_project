d554f4dd8f0442ab4926454fdac73f57b2312ad2
Acshi Haggenmiller <acshikh@umich.edu>
2018-11-26 17:38:36 -0500

    tensorflow version still not working. discriminator pummels generator

--- mnist_gan.py
+++ mnist_gan.py
@@ -18,6 +18,7 @@ tf.enable_eager_execution()
 # Set random seem for reproducibility
 manualSeed = 999
 np.random.seed(manualSeed)
+torch.manual_seed(manualSeed)
 tf.set_random_seed(manualSeed)
 
 def mnist_data():
@@ -53,11 +54,13 @@ discriminator = models.Sequential([
     discriminator_stage1,
     layers.Conv2D(1, 7, use_bias=False, activation="sigmoid"),
 ])
+discriminator_compiled = tf.contrib.eager.defun(discriminator)
 
 classifier = models.Sequential([
     discriminator_stage1,
     layers.Conv2D(10, 7, use_bias=False, activation="sigmoid")
 ])
+classifier_compiled = tf.contrib.eager.defun(classifier)
 
 generator = models.Sequential([
     # We start out with a 1 "pixel" "image" with gen_features "channels"
@@ -76,31 +79,19 @@ generator = models.Sequential([
     layers.Conv2DTranspose(1, 4, strides=2, padding="same", use_bias=False, activation="tanh"),
     # 28x28
 ])
+generator_compiled = tf.contrib.eager.defun(generator)
 
-# print("Has CUDA? {}".format(torch.cuda.is_available()))
-# print("Using {} threads!".format(torch.get_num_threads()))
 print("Discriminator parameter count: {}".format(discriminator.count_params()))
 print("Generator parameter count: {}".format(generator.count_params()))
 print("Additional classifier parameter count: {}".format(classifier.layers[1].count_params()))
 
-# discriminator.compile(optimizer=tf.train.AdamOptimizer(0.0004),
-#                       loss='binary_crossentropy',
-#                       metrics=['accuracy'])
-#
-# classifier.compile(optimizer=tf.train.AdamOptimizer(0.0004),
-#                    loss='categorical_crossentropy',
-#                    metrics=['accuracy'])
-#
-# generator.compile(optimizer=tf.train.AdamOptimizer(0.0004),
-#                   loss='binary_crossentropy',
-#                   metrics=['accuracy'])
-
-def discriminator_loss(real_output, generated_output):
+def discriminator_real_loss(real_output):
     # real images should be labeled as '1'
-    real_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=tf.ones_like(real_output), logits=real_output)
+    return tf.losses.sigmoid_cross_entropy(tf.ones_like(real_output), real_output)
+
+def discriminator_fake_loss(generated_output):
     # fake images should be labeled as '0'
-    generated_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=tf.zeros_like(generated_output), logits=generated_output)
-    return real_loss + generated_loss
+    return tf.losses.sigmoid_cross_entropy(tf.zeros_like(generated_output), generated_output)
 
 def generator_loss(generated_output):
     # fake images should be labeled as '1'
@@ -108,12 +99,45 @@ def generator_loss(generated_output):
 
 def classifier_loss(classifier_output, real_labels):
     # real images should be labeled correctly
-    return tf.losses.sigmoid_cross_entropy(tf.one_hot(real_labels, 10),
+    return tf.losses.softmax_cross_entropy(tf.one_hot(real_labels, 10),
                                            tf.reshape(classifier_output, [-1, 10]))
 
-d_optimizer = tf.train.AdamOptimizer(0.0004)
-g_optimizer = tf.train.AdamOptimizer(0.0004)
-c_optimizer = tf.train.AdamOptimizer(0.0004)
+d_optimizer = tf.train.AdamOptimizer(0.0002)
+g_optimizer = tf.train.AdamOptimizer(0.0002)
+c_optimizer = tf.train.AdamOptimizer(0.0002)
+
+def train_discriminator(real_images):
+    noise = tf.random_normal([batch_size, 1, 1, gen_features])
+    generated_images = generator_compiled(noise, training=True)
+    with tf.GradientTape() as grad_tape:
+        real_output = discriminator_compiled(real_images, training=True)
+        real_loss = discriminator_real_loss(real_output)
+
+        generated_output = discriminator_compiled(generated_images, training=True)
+        fake_loss = discriminator_fake_loss(generated_output)
+        total_loss = real_loss + fake_loss
+    gradients_of_d = grad_tape.gradient(total_loss, discriminator.variables)
+    d_optimizer.apply_gradients(zip(gradients_of_d, discriminator.variables))
+
+    return total_loss.numpy(), real_output, generated_output
+
+def train_classifier(real_images, real_labels):
+    with tf.GradientTape() as grad_tape:
+        classifier_output = classifier_compiled(real_images, training=True)
+        c_loss = classifier_loss(classifier_output, real_labels)
+    gradients_of_c = grad_tape.gradient(c_loss, classifier.layers[1].variables)
+    c_optimizer.apply_gradients(zip(gradients_of_c, classifier.layers[1].variables))
+    return c_loss.numpy()
+
+def train_generator(real_images):
+    with tf.GradientTape() as grad_tape:
+        noise = tf.random_normal([batch_size, 1, 1, gen_features])
+        generated_images = generator_compiled(noise, training=True)
+        generated_output = discriminator_compiled(generated_images, training=True)
+        g_loss = generator_loss(generated_output)
+    gradients_of_g = grad_tape.gradient(g_loss, generator.variables)
+    g_optimizer.apply_gradients(zip(gradients_of_g, generator.variables))
+    return g_loss.numpy()
 
 checkpoint_dir = './training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
@@ -140,28 +164,10 @@ for epoch in range(num_epochs):
         real_labels = real_labels_torch.numpy()
         N = real_images.shape[0]
 
-        noise = tf.random_normal([batch_size, 1, 1, gen_features])
-
-        with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape, tf.GradientTape() as c_tape:
-            generated_images = generator(noise, training=True)
-
-            real_output = discriminator(real_images, training=True)
-            generated_output = discriminator(generated_images, training=True)
-            classifier_output = classifier(real_images, training=True)
-
-            g_loss = generator_loss(generated_output)
-            d_loss = discriminator_loss(real_output, generated_output)
-            c_loss = classifier_loss(classifier_output, real_labels)
-
-        gradients_of_g = g_tape.gradient(g_loss, generator.variables)
-        gradients_of_d = d_tape.gradient(d_loss, discriminator.variables)
-        gradients_of_c = c_tape.gradient(c_loss, classifier.layers[1].variables)
-
-        g_optimizer.apply_gradients(zip(gradients_of_g, generator.variables))
-        d_optimizer.apply_gradients(zip(gradients_of_d, discriminator.variables))
-        c_optimizer.apply_gradients(zip(gradients_of_c, classifier.layers[1].variables))
-
-        logger.log(d_loss.numpy(), g_loss.numpy(), epoch, n_batch, num_batches)
+        d_loss, real_output, generated_output = train_discriminator(real_images)
+        c_loss = train_classifier(real_images, real_labels)
+        g_loss = train_generator(real_images)
+        logger.log(d_loss, g_loss, epoch, n_batch, num_batches)
 
         # Display Progress every few batches
         if n_batch % 10 == 0:
@@ -170,7 +176,7 @@ for epoch in range(num_epochs):
             batch_start_time = now_time
             print("Batches took {:.3f} ms".format(elapsed * 1000))
 
-            test_images = generator(test_noise).numpy()
+            test_images = generator_compiled(test_noise).numpy()
 
             logger.log_images(
                 test_images, num_test_samples,
@@ -179,7 +185,7 @@ for epoch in range(num_epochs):
             # Display status Logs
             logger.display_status(
                 epoch, num_epochs, n_batch, num_batches,
-                d_loss.numpy(), g_loss.numpy(), c_loss.numpy(), real_output.numpy(), generated_output.numpy()
+                d_loss, g_loss, c_loss, real_output.numpy(), generated_output.numpy()
             )
             # save progess
             checkpoint.save(file_prefix = checkpoint_prefix)
